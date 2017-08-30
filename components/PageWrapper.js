be1a4316bdd4a7bedec5518ff954154f07993298
Ezekiel <ezekielaquino@users.noreply.github.com>
2017-08-30 09:59:17 +0200

    Moves cssReset
    - css reset is now on its own file, imported in pageWrapper component

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -1,5 +1,6 @@
 import {Component} from 'react';
 import Head from 'next/head';
+import cssReset from '../components/_cssReset';
 
 /**
  * This component just wraps all views,
@@ -19,57 +20,9 @@ class PageWrapper extends Component {
         {this.props.children}
 
 
+        {/* An example: we can also load stylesheets from another module */}
+        <style jsx global>{cssReset}</style>
         <style jsx global>{`
-          *,
-          *:before,
-          *:after {
-            box-sizing: border-box;
-          }
-          html, body, div, span, applet, object, iframe,
-          h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-          a, abbr, acronym, address, big, cite, code,
-          del, dfn, em, img, ins, kbd, q, s, samp,
-          small, strike, strong, sub, sup, tt, var,
-          b, u, i, center,
-          dl, dt, dd, ol, ul, li,
-          fieldset, form, label, legend,
-          table, caption, tbody, tfoot, thead, tr, th, td,
-          article, aside, canvas, details, embed, 
-          figure, figcaption, footer, header, hgroup, 
-          menu, nav, output, ruby, section, summary,
-          time, mark, audio, video {
-            margin: 0;
-            padding: 0;
-            border: 0;
-            font-size: 100%;
-            font: inherit;
-            vertical-align: baseline;
-            color: inherit;
-            text-decoration: none;
-          }
-          /* HTML5 display-role reset for older browsers */
-          article, aside, details, figcaption, figure, 
-          footer, header, hgroup, menu, nav, section {
-            display: block;
-          }
-          body {
-            line-height: 1;
-          }
-          ol, ul {
-            list-style: none;
-          }
-          blockquote, q {
-            quotes: none;
-          }
-          blockquote:before, blockquote:after,
-          q:before, q:after {
-            content: '';
-            content: none;
-          }
-          table {
-            border-collapse: collapse;
-            border-spacing: 0;
-          }
           html {
             font-size: 20px;
           }
